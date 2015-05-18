b1231c7324118c1db4d3ac4bb7b891e78e4a4a18
Ekechi Ikenna <luccy559slim@gmail.com>
2015-05-18 20:50:07 +0100

    modified form for metrics

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,2 +1,3 @@
 class Metric < ActiveRecord::Base
+  QUOTA = (5..100).step(5).to_a
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150518191355) do
+ActiveRecord::Schema.define(version: 20150518193407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "metrics", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "quota"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "scrade_sessions", force: :cascade do |t|
     t.string   "title"
